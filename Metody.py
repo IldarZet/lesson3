4a5fad24edbd4fd2cacaaba88513a8dd381b0f26
IldarZ <ildariv@ya.ru>
2019-10-17 19:47:00 +0500

    Add info

---
+++ Metody.py
@@ -0,0 +1,11 @@
+# List методы
+# list.append(x), list.count(x), list.sort([key=функция]), list.clear(), list.reverse()
+
+# Dict
+# dict.items(), dict.values(), dict.keys(), dict.update([other]), dict.copy()
+
+# Set
+# set.copy(), set.add(elem), set.remove(elem), set.clear(), set.union(other, ...)
+
+# Str
+# S.split(символ), S.replace(шаблон, замена), len(S), S.join(список)
